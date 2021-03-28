53722569aa45be86a15f510609244c8e148b7950
hgrranzi <vkh.mariia@gmail.com>
2021-03-28 18:31:34 +0300

    Modified the main function

--- main.c
+++ main.c
@@ -8,10 +8,10 @@
 
 int main(int argc, char **argv)
 {
-	if (argc > 1)
-	{
+	if (argc == 2)
 		aka_putnbr(eval_expression(argv[1]));
-		write(1, "\n", 1);
-	}
+	else
+		write(1, "Error", 5);
+	write(1, "\n", 1);
 	return (0);
 }
